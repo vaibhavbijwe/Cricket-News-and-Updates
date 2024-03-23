65c083dcd1f5d653f88e8616ee6deaa99b1563e4
Vaibhav Bijwe <vaibhavbijwe30@gmail.com>
2024-03-23 23:14:51 +0530

    first commit

---
+++ Live-Cricket-Scores-and-Commentary-Backend/src/main/java/com/crick/aps/LiveCricketScoresandCommentaryBackend/LiveCricketScoresAndCommentaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.crick.aps.LiveCricketScoresandCommentaryBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveCricketScoresAndCommentaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveCricketScoresAndCommentaryBackendApplication.class, args);
+	}
+
+}
---
+++ Live-Cricket-Scores-and-Commentary-Backend/src/test/java/com/crick/aps/LiveCricketScoresandCommentaryBackend/LiveCricketScoresAndCommentaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crick.aps.LiveCricketScoresandCommentaryBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveCricketScoresAndCommentaryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
